af083e4f9dab8b131ad52250ceafb7909b355a8f
gigajoona <joona@dublao.com>
2012-03-21 15:38:06 +0200

    Added contact page and refactored Static_pages_spec a bit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  let(:base_title) { "Nerd App" } # setting the base title
+  
   describe "Home page" do
     it "should have the content 'Nerd App'" do
       visit '/static_pages/home'
@@ -12,7 +14,7 @@ describe "StaticPages" do
     it "should have the title Home" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                      :text => "Home | Nerd App")
+                      :text => "Home | #{base_title}")
     end
   end
   
@@ -25,7 +27,7 @@ describe "StaticPages" do
     it "should have the title Help" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                      :text => "Help | Nerd App")
+                      :text => "Help | #{base_title}")
     end
   end
   
@@ -38,8 +40,21 @@ describe "StaticPages" do
       it "should have the title About" do
         visit '/static_pages/about'
         page.should have_selector('title',
-                        :text => "About | Nerd App")
+                        :text => "About | #{base_title}")
       end
       
+    end
+    
+    describe "Contact page" do
+      it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        page.should have_selector('h1', :text => 'Contact')
+      end
+
+      it "should have the title Contact" do
+        visit '/static_pages/contact'
+        page.should have_selector('title',
+                        :text => "Contact | #{base_title}")
+      end
     end
 end

7adce172282a84aad9afa7a035f5a84fd6328397
gigajoona <joona@dublao.com>
2012-03-22 18:16:22 +0200

    Finished user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Joona Tuunanen"
+    email    "joona@dublao.com"
+    password "cukivagy27"
+    password_confirmation "cukivagy27"
+  end
+end

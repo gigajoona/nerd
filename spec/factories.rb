0dcc0f3d2ffa43f5d282a74ae44131109dc6cddb
gigajoona <joona@dublao.com>
2012-03-23 15:52:35 +0200

    Finish user edit, update, index and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Joona Tuunanen"
-    email    "joona@dublao.com"
-    password "cukivagy27"
-    password_confirmation "cukivagy27"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end

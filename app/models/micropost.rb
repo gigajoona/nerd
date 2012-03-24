d9ad44837cab07b902572fd423cd6b1100f7dfad
gigajoona <joona@dublao.com>
2012-03-24 21:22:59 +0200

    tweak in micropost.rb

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,17 +1,12 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content
   belongs_to :user
-  
-  validates :content, presence: true, length: { maximum: 500 }
+
+  validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
   
   default_scope order: 'microposts.created_at DESC'
-  
-  def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids.join(', ')
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
-  end
-  
+
   # Returns microposts from the users being followed by the given user.
   scope :from_users_followed_by, lambda { |user| followed_by(user) }
 
